7cf8597a1d2d05914641f842b8bc6c52ed92c666
LEE HYUN WOO <lhwda1388@naver.com>
2022-03-13 19:42:27 +0900

    physics setting

---
+++ physics/types/global.d.ts
@@ -0,0 +1,30 @@
+declare module '*.png' {
+  const source: string;
+  export default source;
+}
+declare module '*.glsl' {
+  const source: string;
+  export default source;
+}
+
+declare module '*.jpg' {
+  const source: string;
+  export default source;
+}
+
+declare module '*.glb' {
+  const source: string;
+  export default source;
+}
+
+declare module '*.gltf' {
+  const source: string;
+  export default source;
+}
+
+declare global {
+  interface Window {
+    clientWidth: number;
+    offsetWidth: number;
+  }
+}
