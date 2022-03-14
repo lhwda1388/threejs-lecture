7897e798d912e458d2a364d522dc2685ace17b8f
LEE HYUN WOO <lhwda1388@naver.com>
2022-03-14 20:08:30 +0900

    physics 01

--- physics/src/ex01.ts
+++ physics/src/ex01.ts
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as CANNON from 'cannon-es';
 
 // ----- 주제: cannon.js 기본 세팅
 
@@ -40,20 +42,63 @@ export default function example() {
   scene.add(directionalLight);
 
   // Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  // Cannon(물리엔진)
+  const cannonWorld = new CANNON.World();
+  cannonWorld.gravity.set(0, -9.8, 0);
+
+  const floorShape = new CANNON.Plane(); // 지오메트리 같은
+  const floorBody = new CANNON.Body({
+    mass: 0, // 중력 영향 X
+    position: new CANNON.Vec3(0, 0, 0),
+    shape: floorShape,
+  }); // 물리가 적용되는 실체
+  floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI / 2);
+
+  cannonWorld.addBody(floorBody);
+
+  const boxShape = new CANNON.Box(new CANNON.Vec3(0.25, 2.5, 0.25));
+  const boxBody = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 10, 0),
+    shape: boxShape,
+  });
+
+  cannonWorld.addBody(boxBody);
 
   // Mesh
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshStandardMaterial({
+  const floorMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10, 10),
+    new THREE.MeshStandardMaterial({
+      color: 'slategray',
+    }),
+  );
+
+  floorMesh.rotation.x = -Math.PI / 2;
+
+  scene.add(floorMesh);
+
+  const boxGeometry = new THREE.BoxGeometry(0.5, 5, 0.5);
+  const boxMaterial = new THREE.MeshStandardMaterial({
     color: 'seagreen',
   });
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+  boxMesh.position.y = 0.5;
+  scene.add(boxMesh);
 
   // 그리기
   const clock = new THREE.Clock();
 
   function draw() {
     const delta = clock.getDelta();
+
+    // 주사율에 따른 제한
+    let cannonStepTime = 1 / 60;
+    if (delta < 0.01) cannonStepTime = 1 / 120;
+    cannonWorld.step(cannonStepTime, delta, 3);
+    boxMesh.position.copy(boxBody.position as any); // 위치
+    boxMesh.quaternion.copy(boxBody.quaternion as any); // 회전
 
     renderer.render(scene, camera);
     renderer.setAnimationLoop(draw);
